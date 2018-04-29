8ebe177fd80b616ff2da6aaca5e1af4957dd7216
Matěj Melichna <melimahu@gmail.com>
2018-04-29 20:21:15 +0200

    Changed serial communication protocol in Arduino firmware - void sendData

--- sketch_apr28a/sketch_apr28a.ino
+++ sketch_apr28a/sketch_apr28a.ino
@@ -1,9 +1,22 @@
-int a = 0;
+int myArray[7] = {0, 1, 2, 3, 4, 5, 6} ;
 void setup(){
   Serial.begin(9600);
 }
 void loop(){
-  a ++;
-  Serial.println(a);
-  delay(10);
+  sendData(myArray, 7);
+  delay(1000);
+}
+
+void sendData (int dataArray[], int lenght) {
+  for(int n=0; n < lenght; n++){
+    if(n==0){
+      Serial.print(dataArray[n]);
+      Serial.print(" => ");
+    } else if (n == (lenght -1)){
+      Serial.println(dataArray[n]);
+    } else {
+      Serial.print(dataArray[n]);
+      Serial.print(" => ");
+    }
+  }
 }
