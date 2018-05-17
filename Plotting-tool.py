173f9b1ccd897a148ec481fc15243caf6ceb766f
Matěj Melichna <melimahu@gmail.com>
2018-05-17 15:27:12 +0200

    Working releaseof Object-Reworked Plotting-tool.py

--- Plotting-tool.py
+++ Plotting-tool.py
@@ -54,38 +54,56 @@ class parser:
     def close(self):
         self.inputFile.close()
 
+class graphComposer:
 
+    def __init__(self, pathStr, XArray, YArray):
+        self.pathStr = pathStr
+        self.XArray = XArray
+        self.YArray = YArray
+        figArray = self.pathStr.split("/")
+        self.figName = figArray[len(figArray) - 1]
+    def composeLabels(self, XText, XUnit, YText, YUnit):
+        self.XLabel = str(XText + " " + "[" + XUnit + "]")
+        self.YLabel = str(YText + " " + "[" + YUnit + "]")
+    def composeTitle(self, title):
+        self.title = str(title)
+    def composeLegend(self):
+        self.legendText = str("Source: " + self.figName)
+    def generateGraph(self):
+        plt.xlabel(self.XLabel)
+        plt.ylabel(self.YLabel)
+        plt.title(self.title)
+        plt.plot(self.XArray, self.YArray, label = self.legendText)
+        plt.legend()
+        currentFig = plt.gcf()
+        plt.show()
+        fileName = self.figName.split(".")
+        name = fileName[0]
+        currentFig.savefig(name)
 
-
-
-
-
-pathsFile = configFile("/home/melimat/Documents/CO2/Paths.txt")
-pathsFile.open()
-pathsArray = pathsFile.pathsToArray()
-nameArray = pathsFile.fileNames()
-pathsFile.close()
-
-
-i = int(0)
-for eachElement in pathsArray:
-    dirArray = eachElement.split("\n")
-    directory = dirArray[0]
-    print (directory)
-    parse = parser(directory)
-    parse.open()
-    plotArray = parse.parseData()
-    unitsArray = parse.units()
-    plt.xlabel(unitsArray[0])
-    plt.ylabel(unitsArray[1])
-    XArray = plotArray[0]
-    YArray = plotArray[1]
-    plt.plot(XArray, YArray, label = "Source: " + nameArray[i] )
-    plt.legend()
-    #plt.show()
-    picNameArray = nameArray[i].split(".")
-    picName = picNameArray[0]
-    plt.savefig(picName)
-    parse.close()
-    i += 1
-
+def main():
+	pathsFile = configFile("/home/melimat/Documents/CO2/Paths.txt")
+	pathsFile.open()
+	pathsArray = pathsFile.pathsToArray()
+	nameArray = pathsFile.fileNames()
+	pathsFile.close()
+	
+	
+	i = int(0)
+	for eachElement in pathsArray:
+	    dirArray = eachElement.split("\n")
+	    directory = dirArray[0]
+	    print (directory)
+	    parse = parser(directory)
+	    parse.open()
+	    plotArray = parse.parseData()
+	    unitsArray = parse.units()
+	    parse.close()
+	    graph = graphComposer(nameArray[i], plotArray[0], plotArray[1])
+	    graph.composeLabels("Time", unitsArray[0], "CO2 Concentration", unitsArray[1])
+	    graph.composeTitle("Graph of concentration of CO2")
+	    graph.composeLegend()
+	    graph.generateGraph()
+	    i += 1
+	
+main()
