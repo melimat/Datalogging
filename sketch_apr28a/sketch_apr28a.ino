55b0e4a2ecc968b7c2c57d85bdcd8d46aa3d495b
Matěj Melichna <2017-d-melichna@KNIHOVNA-2.GARAB>
2018-05-21 08:39:04 +0200

    Improved Arduino firmware for reading data from sensors and sending them to computer

--- sketch_apr28a/sketch_apr28a.ino
+++ sketch_apr28a/sketch_apr28a.ino
@@ -1,11 +1,18 @@
-int myArray[7] = {
-  0, 1, 2, 3, 4, 5, 6} 
-;
+const int CO2Pin = A0;
+const int LDRPin = A1;
+const int constant1 = 1
+const int constant2 = 1
+
 void setup(){
   Serial.begin(9600);
+  pinMode(CO2Pin, INPUT)
+  pinMode(LDRPin, INPUT)
 }
 void loop(){
-  sendData(myArray, 7);
+  CO2Concentration = analogRead(CO2Pin)/constant2
+  LightIntensity = analogRead(LDRPin)/consant1
+  myArray[2] = {CO2Concentration, LightIntensity}
+  sendData(myArray, 2);
   delay(1000);
 }
 
@@ -14,14 +21,13 @@ void sendData (int dataArray[], int lenght) {
     if(n==0){
       Serial.print(dataArray[n]);
       Serial.print(" => ");
-    } 
+    }
     else if (n == (lenght -1)){
       Serial.println(dataArray[n]);
-    } 
+    }
     else {
       Serial.print(dataArray[n]);
       Serial.print(" => ");
     }
   }
 }
-
